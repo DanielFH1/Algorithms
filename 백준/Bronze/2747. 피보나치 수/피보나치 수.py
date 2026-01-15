8115fc88b8f1b3c434d0592a9b8d0a2bad460454
Seungwoo Lim <daniel73919@gmail.com>
2026-01-15 12:06:45 +0900

    [Bronze II] Title: 피보나치 수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+def solve_normal(n):
+    fib = [0,1]
+    
+    for i in range(2,n+1):
+        fib.append(fib[i-1] + fib[i-2])
+        
+    return fib[n]
+print(solve_normal(n))
