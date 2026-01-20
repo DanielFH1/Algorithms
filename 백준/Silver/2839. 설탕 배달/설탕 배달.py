cfa6ea61b25660a59219246164731f68d0abd60e
Seungwoo Lim <daniel73919@gmail.com>
2026-01-20 23:42:01 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,16 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+
+count = 0
+
+while N>=0:
+    if N % 5 == 0:
+        count += (N //5)
+        print(count)
+        break
+    N -= 3
+    count += 1
+
+if N <0:
+    print(-1)
